c8169dd0ead57d9ae5aa27b35ea6d2524a235347
Blake Uribe <blakeuribe15@gmail.com>
2025-02-03 12:30:23 -0800

    Optimization

--- utils/finance_utils.py
+++ utils/finance_utils.py
@@ -6,6 +6,52 @@ from numpy.typing import NDArray
 
 # If Updated remember to adjust date at bottom
 
+def get_corr_pairs_of_stocks(tickers: list) -> pd.DataFrame:
+    """
+    Computes and returns the correlation pairs of given stock tickers.
+
+    Args:
+        tickers (list): A list of stock tickers.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing correlation pairs sorted in ascending order.
+    """
+    
+    # Download adjusted closing prices for the past year
+    grouped_sector_data = yf.download(tickers, period='1y', auto_adjust=True)['Close']
+    correlation_matrix = grouped_sector_data.corr()
+    corr_pairs = correlation_matrix.stack()
+
+    # Remove self-correlations (where ticker_1 == ticker_2)
+    corr_pairs = corr_pairs[corr_pairs.index.get_level_values(0) != corr_pairs.index.get_level_values(1)]
+
+    # Format index to be "Ticker1_Ticker2"
+    corr_pairs.index = corr_pairs.index.map('_'.join)
+    
+    # Convert to DataFrame and rename columns
+    corr_df = corr_pairs.to_frame(name='Correlation')
+
+    # Drop duplicate correlation values (since correlation is symmetric)
+    corr_df = corr_df.drop_duplicates(subset=['Correlation'])
+
+    # Sort by correlation value in ascending order
+    return corr_df.sort_values(by='Correlation', ascending=True)
+
+
+def get_top_n_by_sector(df: pd.DataFrame, filter_var: str, top_n: int = 3) -> pd.DataFrame:
+    """
+    Returns the top N companies per sector based on a specified financial metric.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing stock valuation data, including 'sector'.
+        filter_var (str): The column name to filter by (e.g., 'profitMargins').
+        top_n (int, optional): Number of top companies to return per sector. Defaults to 3.
+
+    Returns:
+        pd.DataFrame: DataFrame with the top N companies per sector sorted by the given metric.
+    """
+    return df.groupby('sector').apply(lambda x: x.nlargest(top_n, filter_var)).reset_index(drop=True)
+
 
 # Vectorized function to calculate Sharpe ratio for multiple tickers
 def calculate_sharpe_ratio(tickers: NDArray, tbill: pd.DataFrame, start_date: datetime, end_date: datetime) -> float:
@@ -42,6 +88,6 @@ def get_sector(ticker: str) -> str:
 
 
 print('\n---------------------------------')
-print('finance_utils.py successfully loaded, updated last Jan. 15 2025')
+print('finance_utils.py successfully loaded, updated last Jan. 27 2025')
 print('---------------------------------')
 print('\n')
